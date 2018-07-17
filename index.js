433c750a6e562e9f38b31e79b796dd7a2e346828
Stefanie Torossian <stefanietorossian@gmail.com>
2018-07-17 02:45:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function happyHolidays(string) {
   return "Happy holidays!"
 }
   
-  function happyHolidaysTo(name) 
-  {
+  function happyHolidaysTo(name) {
+    if 
     console.log ("Happy holidays, ${name}!");
   }
